bc7262cd015caccdb519f7df4a9b5a5ef9283e75
Yuan Lunchuan <799652013@qq.com>
2017-07-29 08:46:06 +0800

    First commit

--- spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/controllers/MainController.java
+++ spring-boot-mysql-springdatajpa-hibernate/src/main/java/netgloo/controllers/MainController.java
@@ -2,16 +2,13 @@ package netgloo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-  @RequestMapping("/")
-  @ResponseBody
+  @RequestMapping(value="/home")
   public String index() {
-    return "Proudly handcrafted by " +
-        "<a href='http://netgloo.com/en'>Netgloo</a> :)";
+    return "home";
   }
 
 }
